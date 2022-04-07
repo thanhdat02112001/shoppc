68963b9bf6a8ea6e74b076f984199a11b70173d6
thanhdat02112001 <77434819+thanhdat02112001@users.noreply.github.com>
2022-04-07 10:37:06 +0700

    Update CategoryController.php

--- app/Http/Controllers/Product/CategoryController.php
+++ app/Http/Controllers/Product/CategoryController.php
@@ -8,7 +8,6 @@ use App\Http\Requests\EditRequest;
 use App\Models\Category;
 use Illuminate\Contracts\Validation\Rule;
 use Illuminate\Http\Request;
-//use Illuminate\Validation\Rule;
 
 class CategoryController extends Controller
 {
@@ -18,13 +17,13 @@ class CategoryController extends Controller
         [
             "name" => $request->name,
             "slug" => $request->slug,
-            "description" => $request->description
+            "description" => $request->description,
         ];
         if (Category::create($formValue)) {
-            $request->session()->flash("success","Cập nhật danh mục thành công!");
+            $request->session()->flash("success", "Cập nhật danh mục thành công!");
             return redirect("/admin/categories");
         } else {
-            $request->session()->flash("error","Đã xảy ra lỗi!");
+            $request->session()->flash("error", "Đã xảy ra lỗi!");
             return redirect("/admin/categories");
         }
     }
@@ -32,24 +31,24 @@ class CategoryController extends Controller
     public function show()
     {
         $categories = Category::all();
-        return view("backend.category",compact('categories'));
+        return view("backend.category", compact('categories'));
     }
 
     public function edit($id)
     {
         $categories = Category::all()->except($id);
         $category = Category::find($id);
-        return view("backend.editcategory",compact('category','categories'));
+        return view("backend.editcategory", compact('category', 'categories'));
     }
 
     public function update($id, EditRequest $request)
     {
         $category = Category::find($id);
         if ($category->update($request->all())) {
-            $request->session()->flash("success","Đã cap nhat danh mục thành công!");
+            $request->session()->flash("success", "Đã cap nhat danh mục thành công!");
             return redirect("/admin/categories");
         } else {
-            $request->session()->flash("error","Đã xảy ra lỗi!");
+            $request->session()->flash("error", "Đã xảy ra lỗi!");
             return redirect("/admin/categories");
         }
     }
